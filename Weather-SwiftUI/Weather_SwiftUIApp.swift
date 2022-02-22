3072a2c3f0646a3e1bb2a2da0d28ba776e17040b
Петухов Василий Сергеевич <vspetukhov@sberbank.ru>
2022-02-22 09:20:49 +0300

    Initial Commit

---
+++ Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Петухов Василий Сергеевич on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
